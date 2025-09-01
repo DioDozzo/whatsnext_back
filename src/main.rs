99281afb0647676871f821c0edc8f8419dc8eac2
GiGGioSo <gioele.pettarini@gmail.com>
2025-09-01 19:45:26 +0200

    added cors and buongiorno

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::{Serialize, json::Json};
+use rocket_cors::CorsOptions;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Serialize)]
+struct Greetings {
+    buongiorno: String
+}
+
+#[get("/buongiorno/<name>")]
+fn specific_buongiorno(name: &str) -> Json<Greetings> {
+    Json(Greetings{
+        buongiorno: name.to_string()
+    })
+}
+
+#[get("/buongiorno")]
+fn generic_buongiorno() -> Json<Greetings> {
+    Json(Greetings{
+        buongiorno: "principessa".to_string()
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let cors = CorsOptions::default().to_cors().expect("Failed to create CORS fairing.");
+    rocket::build().mount("/", routes![
+        specific_buongiorno,
+        generic_buongiorno
+    ]).attach(cors)
 }
